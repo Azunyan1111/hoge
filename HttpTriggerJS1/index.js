af6e390ad915e4f7a31809f6acb80e2cd34b149e
Azunyan1111 <s15a1302@bene.fit.ac.jp>
2018-08-17 16:16:26 +0900

    add

---
+++ HttpTriggerJS1/index.js
@@ -0,0 +1,28 @@
+module.exports = function(context, event) {
+
+    // context.log("event: %j", event);
+    const rawText = event.body.split('&').find((pair) => {
+        return pair.startsWith('text=');
+    });
+    const userName = event.body.split('&').find((pair) => {
+        return pair.startsWith('user_name=');
+    });
+
+    context.log("userName: %j", userName);
+    if (rawText === undefined || userName === undefined || userName === 'user_name=slackbot') {
+        context.done();
+        return;
+    }
+
+    const text = decodeURIComponent(rawText.substr(5));
+    context.log("text:%s", text);
+
+    // この辺でいろいろやってみる
+    context.res = {
+        body: {
+            text: "Hello " + text + "@ GitHub"
+        }
+    };
+    // context.bindngs.outputDocument = {text};
+    context.done();
+};
